d31f43ac0ade84c7611266df895b8a16355b4a02
IPPM-tech <emmanuelolaxx5555@gmail.com>
2026-07-25 18:35:08 +0100

    Add more images to the slider
    const images = [
        "images/hero1.jpg",
        "images/hero2.jpg",
        "images/hero3.jpg",
        "images/hero4.jpg",
        "images/hero5.jpg",
        "images/hero6.jpg",
        "images/hero7.jpg",
        "images/hero8.jpg"
    ];
    
    Yes, I added more heroes to the 5 originally suggested by deep knowledge...

--- js/slider.js
+++ js/slider.js
@@ -14,12 +14,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Images to rotate
     const images = [
-        "images/hero1.jpg",
-        "images/hero2.jpg",
-        "images/hero3.jpg",
-        "images/hero4.jpg",
-        "images/hero5.jpg"
-    ];
+    "images/hero1.jpg",
+    "images/hero2.jpg",
+    "images/hero3.jpg",
+    "images/hero4.jpg",
+    "images/hero5.jpg",
+    "images/hero6.jpg",
+    "images/hero7.jpg",
+    "images/hero8.jpg"
+];
 
     let current = 0;
 
